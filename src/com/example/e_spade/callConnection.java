379d44e0b2267ad66a2d3bbf5b2ebfda34bd8328
piyush aggarwal <mailme_piyushaggarwal@yahoo.in>
2013-04-20 00:27:30 +0530

    image change on button click

--- src/com/example/e_spade/callConnection.java
+++ src/com/example/e_spade/callConnection.java
@@ -1,5 +1,8 @@
 package com.example.e_spade;
 
 public class callConnection {
+	
+	//EditText EditTextName=findViewById(R.id.EditText01);
+	//((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(EditTextName), 0);
 
 }
