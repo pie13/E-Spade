8d9d89b53224987515d0a162415eb3b1387d16cb
piyush aggarwal <mailme_piyushaggarwal@yahoo.in>
2013-04-19 17:01:29 +0530

    activity skeleton

--- gen/com/example/e_spade/R.java
+++ gen/com/example/e_spade/R.java
@@ -28,6 +28,8 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int callconnection=0x7f030001;
+        public static final int homelayout=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
---
+++ src/com/example/e_spade/loading.java
@@ -0,0 +1,5 @@
+package com.example.e_spade;
+
+public class loading {
+
+}
